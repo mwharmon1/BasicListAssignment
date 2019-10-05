2076957f5c3e4210c6fe5f024b6bdc9b2f0f38a9
Michael Harmon <mwharmon1@dmacc.edu>
2019-10-05 18:46:21 -0500

    Initial Commit

---
+++ fun_with_collections/basic_list.py
@@ -0,0 +1,26 @@
+"""
+Author: Michael Harmon
+Date: 10/05/2019
+Description: This program will ask for user input and display
+the input in a list.
+"""
+
+
+def make_list():
+    """
+    this function will call get_input()
+    :return: list
+    """
+    pass
+
+
+def get_input():
+    """
+    this function will prompt user for input
+    :return: string input
+    """
+    pass
+
+
+if __name__ == '__main__':
+    make_list()
