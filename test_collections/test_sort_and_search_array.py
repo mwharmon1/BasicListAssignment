e5e1c981a06b255cbfbe5bf035b2a05e68a5b265
Michael Harmon <mwharmon1@dmacc.edu>
2019-10-07 23:15:29 -0500

    Added test for sort_array. It is failing

--- test_collections/test_sort_and_search_array.py
+++ test_collections/test_sort_and_search_array.py
@@ -3,11 +3,14 @@ from fun_with_collections import sort_and_search_array as a
 
 
 class MyTestCase(unittest.TestCase):
-    def test_search_list_item_found(self):
+    def test_search_array_item_found(self):
         self.assertEqual(a.search_array([8, 6, 'Hi'], 'Hi'), 2)
 
-    def test_search_list_item_not_found(self):
+    def test_search_array_item_not_found(self):
         self.assertEqual(a.search_array([9, 1, 4], 7), -1)
+
+    def test_sort_array(self):
+        self.assertEqual(a.sort_array(['Hello', 4, 1]), 2)
 
 
 if __name__ == '__main__':
