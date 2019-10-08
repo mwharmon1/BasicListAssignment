6f6c28600b7a5d1aebe2ca031feeca48e229d69b
Michael Harmon <mwharmon1@dmacc.edu>
2019-10-07 23:02:09 -0500

    Added 2 new files, sort_and_search_array and test_sort_and_search_array. Added two tests for item found and item not found. Both currently failing.

---
+++ fun_with_collections/sort_and_search_array.py
@@ -0,0 +1,19 @@
+"""
+Author: Michael Harmon
+Date: 10/07/2019
+Description: This program will search and sort an array using
+the sort_array and search_array functions.
+"""
+
+
+def sort_array(my_array):
+    pass
+
+
+def search_array(my_array, my_number):
+    pass
+
+
+if __name__ == '__main__':
+    sort_array()
+    search_array()
---
+++ test_collections/test_sort_and_search_array.py
@@ -0,0 +1,14 @@
+import unittest
+from fun_with_collections import sort_and_search_array as a
+
+
+class MyTestCase(unittest.TestCase):
+    def test_search_list_item_found(self):
+        self.assertEqual(a.search_array([8, 6, 3], 8), 0)
+
+    def test_search_list_item_not_found(self):
+        self.assertEqual(a.search_array([9, 1, 4], 7), -1)
+
+
+if __name__ == '__main__':
+    unittest.main()
