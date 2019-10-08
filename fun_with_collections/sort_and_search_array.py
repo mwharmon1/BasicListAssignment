da81745c556b2742866b39b999db59874005f1e6
Michael Harmon <mwharmon1@dmacc.edu>
2019-10-07 23:13:31 -0500

    Added functionality for search_array. Both tests are now passing

--- fun_with_collections/sort_and_search_array.py
+++ fun_with_collections/sort_and_search_array.py
@@ -4,16 +4,24 @@ Date: 10/07/2019
 Description: This program will search and sort an array using
 the sort_array and search_array functions.
 """
+import array as arr
 
 
-def sort_array(my_array):
+def sort_array(array_to_sort):
     pass
 
 
-def search_array(my_array, my_number):
-    pass
+def search_array(array_to_search, my_number):
+    try:
+        index_of_number = array_to_search.index(my_number)
+    except ValueError:
+        return -1
+    else:
+        return index_of_number
+    # pass
 
 
 if __name__ == '__main__':
-    sort_array()
-    search_array()
+    my_array = arr.array = [1, 2, 'Test']
+    sort_array(my_array)
+    print(search_array(my_array, 'Test'))
--- test_collections/test_sort_and_search_array.py
+++ test_collections/test_sort_and_search_array.py
@@ -4,7 +4,7 @@ from fun_with_collections import sort_and_search_array as a
 
 class MyTestCase(unittest.TestCase):
     def test_search_list_item_found(self):
-        self.assertEqual(a.search_array([8, 6, 3], 8), 0)
+        self.assertEqual(a.search_array([8, 6, 'Hi'], 'Hi'), 2)
 
     def test_search_list_item_not_found(self):
         self.assertEqual(a.search_array([9, 1, 4], 7), -1)
